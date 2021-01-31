920759c2d5fb734e9034765a94f0b7cb57bc2301
MadMax49 <47071544+MadMax49@users.noreply.github.com>
2021-01-31 11:01:52 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -57,10 +57,10 @@ function App() {
           </div>
           <div className="footerbuttons">
             {/* w3schools stuff */}
-            <a href="https://www.youtube.com/channel/UCSSbZRKHS72VuwhjX0-APKg" className="fa fa-youtube" alt="YouTube" target="_blank" rel="noreferrer"></a>
-            <a href="https://www.instagram.com/rwc_2021/" className="fa fa-instagram" alt="Instagram" target="_blank" rel="noreferrer"></a>
-            <a href="https://twitter.com/FacultyRwc" className="fa fa-twitter" alt="Twitter" target="_blank" rel="noreferrer"></a>
-            <a href="https://www.facebook.com/joana.doe.334" className="fa fa-facebook" alt="Facebook" target="_blank" rel="noreferrer"></a>
+            <a href="https://www.youtube.com/channel/UCSSbZRKHS72VuwhjX0-APKg" className="fa fa-youtube" alt="YouTube" target="_blank" rel="noreferrer">&#8205;</a>
+            <a href="https://www.instagram.com/rwc_2021/" className="fa fa-instagram" alt="Instagram" target="_blank" rel="noreferrer">&#8205;</a>
+            <a href="https://twitter.com/FacultyRwc" className="fa fa-twitter" alt="Twitter" target="_blank" rel="noreferrer">&#8205;</a>
+            <a href="https://www.facebook.com/joana.doe.334" className="fa fa-facebook" alt="Facebook" target="_blank" rel="noreferrer">&#8205;</a>
           </div>
           <div className="copyright">
             <h6>Help.RWC@gmail.com </h6>
